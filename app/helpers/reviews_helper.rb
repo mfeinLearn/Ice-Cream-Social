14277e4d113f562f21f4444271a031c1c0a14de6
github username <github email address>
2019-06-16 09:31:51 -0400

    video 4

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,9 @@
 module ReviewsHelper
+  def display_helper
+    if params[:ice_cream_id]
+      h1.tag("Add a Review for #{@review.ice_cream.flavor} - #{@review.ice_cream.brand.name}")
+    else
+      content_tag(:h1, "Create a review")
+    end
+  end
 end
