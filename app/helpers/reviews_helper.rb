01e1606f70d34e52e2490664f3130965916f7a83
github username <github email address>
2019-06-16 09:36:47 -0400

    adding helper to view

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,7 +1,7 @@
 module ReviewsHelper
-  def display_helper
+  def display_helper(review)
     if params[:ice_cream_id]
-      h1.tag("Add a Review for #{@review.ice_cream.flavor} - #{@review.ice_cream.brand.name}")
+      h1.tag("Add a Review for #{review.ice_cream.flavor} - #{review.ice_cream.brand.name}")
     else
       content_tag(:h1, "Create a review")
     end
